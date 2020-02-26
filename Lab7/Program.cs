0bb702ba9aa1552fe14fafb0fad7d6a9f91a44d5
Nikita Maslov <55882052+ImNtail@users.noreply.github.com>
2020-02-26 19:47:45 +0300

    Add files via upload

---
+++ Lab7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int select;
+            Console.Write("Choose the task from 1 to 3: ");
+            select = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            switch (select)
+            {
+                case 1:
+                    Console.WriteLine("The first task: \n");
+                    First.Execute();
+                    break;
+                case 2:
+                    Console.WriteLine("The second task: \n");
+                    Second.Execute();
+                    break;
+                case 3:
+                    Console.WriteLine("The third task: \n");
+                    Third.Execute();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
